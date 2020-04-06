7c5c53932242925a9f729c3c1bb48ee70d0d7719
onizka1117 <hwa1117@naver.com>
2020-04-06 10:15:45 +0900

    vendor

--- script/ui_common.js
+++ script/ui_common.js
@@ -1,1 +1,11 @@
-var test;
+$(function(){
+  // datepicker
+  $( ".ip_date input" ).datepicker({
+    showOn: "button",
+    buttonImage: "../../images/ip_date.png",
+    buttonImageOnly: true,
+    buttonText: "Select date",
+    dateFormat: "yy/mm/dd"
+  });
+
+});
