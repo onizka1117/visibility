338d86c2f47858fdbcd263bd68e0f864f5785dff
onizka1117 <hwa1117@naver.com>
2020-04-24 16:25:44 +0900

    input del

--- script/ui_common.js
+++ script/ui_common.js
@@ -28,30 +28,35 @@ $(function(){
 (function($){
   // 파일명 셋팅
   $.fn.fileReady = function() {
-    $el = $(this);
-    $el.find("input[type=file]").on("change", function(e){
-      var val = $(this).val();
-      $(this).parent().prev().val(val);
+    this.each(function(i){
+      $el = $(this);
+      $el.find("input[type=file]").on("change", function(e){
+        var val = $(this).val();
+        $(this).parent().prev().val(val);
+      });
     });
   };
 
   // 텍스트삭제
   $.fn.inputTextFocus = function() {
-    var $el = $(this);
-    var $ip = $el.find(".ip_txt");
-    valCheck($ip.val());
-    $ip.on("change keyup paste", function(e){
-      var val = $(this).val();
-      valCheck(val);
+    this.each(function(i){
+      var $el = $(this);
+      var $ip = $el.find(".ip_txt");
+      valCheck($ip.val());
+      $ip.on("change keyup paste", function(e){
+        var val = $(this).val();
+        valCheck(val);
+      });
+      $el.find(".btn_txt_del").on("click", function(e){
+        $ip.val("");
+        $el.removeClass("focus");
+        e.preventDefault();
+      });
+      function valCheck(val){
+        val == "" ? $el.removeClass("focus") : $el.addClass("focus");
+      }
     });
-    $el.find(".btn_txt_del").on("click", function(e){
-      $ip.val("");
-      $el.removeClass("focus");
-      e.preventDefault();
-    });
-    function valCheck(val){
-      val == "" ? $el.removeClass("focus") : $el.addClass("focus");
-    }
+
   };
 
 })(jQuery);
